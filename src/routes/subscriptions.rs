34f47f5a2dd1d71f7d744d06cb9c15f91ce9b44e
Daniel Furman <djfurman@gmail.com>
2023-11-11 14:41:37 -0500

    Refactor database statement to it's own function. Instrument database action and controller action with tracing::instrument.

--- src/routes/subscriptions.rs
+++ src/routes/subscriptions.rs
@@ -1,7 +1,6 @@
 use actix_web::{web, HttpResponse};
 use chrono::Utc;
-use sqlx::{Pool, Postgres};
-use tracing::Instrument;
+use sqlx::{PgPool, Pool, Postgres};
 use uuid::Uuid;
 
 #[derive(serde::Deserialize)]
@@ -10,28 +9,12 @@ pub struct FormData {
     name: String,
 }
 
-pub async fn subscribe(
-    form: web::Form<FormData>,
-    db_pool: web::Data<Pool<Postgres>>,
-) -> HttpResponse {
-    let request_id = Uuid::new_v4();
-    let request_span = tracing::info_span!(
-        "Adding a new subscriber.",
-        %request_id,
-        subscriber_email = %form.email,
-        subscriber_name = %form.name
-    );
-    // This is a known bad idea
-    let _request_span_guard = request_span.enter();
-    tracing::info!(
-        "request_id {} - Adding '{}' '{}' as a new subscriber",
-        request_id,
-        form.email,
-        form.name
-    );
-    // Slime out an insert statement to the database, will add validations
-    let query_span = tracing::info_span!("Saving new subscriber details in the database");
-    match sqlx::query!(
+#[tracing::instrument(
+    name = "Saving new subscriber details in the database",
+    skip(form, db_pool)
+)]
+pub async fn insert_subscriber(db_pool: &PgPool, form: &FormData) -> Result<(), sqlx::Error> {
+    sqlx::query!(
         r#"
         Insert Into subscriptions(id, email, name, subscribed_at)
         Values ($1, $2, $3, $4)
@@ -41,23 +24,34 @@ pub async fn subscribe(
         form.name,
         Utc::now()
     )
-    .execute(db_pool.as_ref())
-    .instrument(query_span)
+    .execute(db_pool)
     .await
-    {
-        Ok(_) => {
-            tracing::info!(
-                "request_id {} - New subscriber details have been saved",
-                request_id
-            );
-            HttpResponse::Ok().finish()
-        }
+    .map_err(|e| {
+        tracing::error!("Failed to execute query: {:?}", e);
+        e
+        // Using the `?` operator to return early if the function failed, returning `sqlx::Error`
+    })?;
+    Ok(())
+}
+
+#[tracing::instrument(
+    name = "Adding a subscriber",
+    skip(form, db_pool),
+    fields(
+        request_id = %Uuid::new_v4(),
+        subscriber_email = %form.email,
+        subscriber_name = %form.name
+    )
+)]
+pub async fn subscribe(
+    form: web::Form<FormData>,
+    db_pool: web::Data<Pool<Postgres>>,
+) -> HttpResponse {
+    // Slime out an insert statement to the database, will add validations
+    match insert_subscriber(&db_pool, &form).await {
+        Ok(_) => HttpResponse::Ok().finish(),
         Err(e) => {
-            tracing::error!(
-                "request_id {} - Failed to execute query: {:?}",
-                request_id,
-                e
-            );
+            tracing::error!("Failed to execute query: {:?}", e);
             HttpResponse::InternalServerError().finish()
         }
     }
