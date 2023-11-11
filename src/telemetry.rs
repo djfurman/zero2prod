33ee045a4322ca82978477d5aa2f129c13d9b146
Daniel Furman <djfurman@gmail.com>
2023-11-11 14:16:50 -0500

    Add static definition for setting CI test logging.

--- src/telemetry.rs
+++ src/telemetry.rs
@@ -2,15 +2,23 @@ use tracing::subscriber::set_global_default;
 use tracing::Subscriber;
 use tracing_bunyan_formatter::{BunyanFormattingLayer, JsonStorageLayer};
 use tracing_log::LogTracer;
-use tracing_subscriber::{layer::SubscriberExt, EnvFilter, Registry};
+use tracing_subscriber::{fmt::MakeWriter, layer::SubscriberExt, EnvFilter, Registry};
 
 /// Compose multiple layers into `tracing`'s subscriber
-pub fn get_subscriber(name: String, env_filter: String) -> impl Subscriber + Send + Sync {
+pub fn get_subscriber<Sink>(
+    name: String,
+    env_filter: String,
+    sink: Sink,
+) -> impl Subscriber + Send + Sync
+where
+    // This "weird" syntax is a high-ranked trait bound (HRTB)
+    Sink: for<'a> MakeWriter<'a> + Send + Sync + 'static,
+{
     // Print all spans at += INFO level if the RUST_LOG variable is not set
     let env_filter =
         EnvFilter::try_from_default_env().unwrap_or_else(|_| EnvFilter::new(env_filter));
     // Output the span to stdout
-    let formatting_layer = BunyanFormattingLayer::new(name, std::io::stdout);
+    let formatting_layer = BunyanFormattingLayer::new(name, sink);
     // The `with` method is provided by `SubscriberExt`, an extension trait for `Subscriber exposted by `tracing_subscriber`
     Registry::default()
         .with(env_filter)
