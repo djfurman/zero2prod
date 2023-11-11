69bb8cd5fb3060f62e0faed842d10ee17fdb60f2
Daniel Furman <djfurman@gmail.com>
2023-11-11 15:14:18 -0500

    Remove ideomatic error in unnecessary borrow from the configuration  type.

--- src/main.rs
+++ src/main.rs
@@ -13,7 +13,7 @@ async fn main() -> Result<(), std::io::Error> {
     // Panic if we cannot read configuration
     let configuration = get_configuration().expect("Failed to read configuration");
 
-    let pool = PgPool::connect(&configuration.database.connection_string().expose_secret())
+    let pool = PgPool::connect(configuration.database.connection_string().expose_secret())
         .await
         .expect("Failed to connect to Postgres database");
 
