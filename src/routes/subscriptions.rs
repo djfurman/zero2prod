9eda608aff85986a2b24f25fdca86b18b8284ddf
Daniel Furman <djfurman@gmail.com>
2023-11-24 12:50:07 -0500

    Add faker logic to handle many fake email addresses rather than a static test. Add domain logic to handle email addresses. Modify handler to ensure that correct name and email are paresd from the form.

--- src/routes/subscriptions.rs
+++ src/routes/subscriptions.rs
@@ -3,7 +3,7 @@ use chrono::Utc;
 use sqlx::{PgPool, Pool, Postgres};
 use uuid::Uuid;
 
-use crate::domain::{NewSubscriber, SubscriberName};
+use crate::domain::{NewSubscriber, SubscriberEmail, SubscriberName};
 
 #[derive(serde::Deserialize)]
 pub struct FormData {
@@ -25,7 +25,7 @@ pub async fn insert_subscriber(
         Values ($1, $2, $3, $4)
         "#,
         Uuid::new_v4(),
-        new_subscriber.email,
+        new_subscriber.email.as_ref(),
         new_subscriber.name.as_ref(),
         Utc::now()
     )
@@ -56,8 +56,13 @@ pub async fn subscribe(
         // Return Early if the name is invalid, with a 400
         Err(_) => return HttpResponse::BadRequest().finish(),
     };
+    let email = match SubscriberEmail::parse(form.0.email) {
+        Ok(name) => name,
+        // Return Early if the name is invalid, with a 400
+        Err(_) => return HttpResponse::BadRequest().finish(),
+    };
     let new_subscriber = NewSubscriber {
-        email: form.0.email,
+        email: email,
         name: name,
     };
     match insert_subscriber(&db_pool, &new_subscriber).await {
