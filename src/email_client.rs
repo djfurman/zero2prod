ee476e9ccda29bb11b38b70080e90f6814fd88a9
Daniel Furman <djfurman@gmail.com>
2023-12-16 15:57:00 -0500

    Add testing for email based API client to confirm subscriptions. Use fake crate to generate fake but realistic data.

---
+++ src/email_client.rs
@@ -0,0 +1,107 @@
+use reqwest::{Client, Url};
+use secrecy::{ExposeSecret, Secret};
+
+use crate::domain::SubscriberEmail;
+
+#[derive(Clone)]
+pub struct EmailClient {
+    authorization_token: Secret<String>,
+    http_client: Client,
+    base_url: String,
+    sender: SubscriberEmail,
+}
+
+#[derive(serde::Serialize)]
+struct SendEmailRequest {
+    from: String,
+    to: String,
+    subject: String,
+    html_body: String,
+    text_body: String,
+}
+
+impl EmailClient {
+    pub async fn send_email(
+        &self,
+        recipient: SubscriberEmail,
+        subject: &str,
+        html_content: &str,
+        text_content: &str,
+    ) -> Result<(), reqwest::Error> {
+        let email_base_url = Url::parse(&self.base_url).expect("Error fetching email base url.");
+        let url = email_base_url
+            .join("/email")
+            .expect("Error computing email API url.");
+
+        let payload = SendEmailRequest {
+            from: self.sender.as_ref().to_owned(),
+            to: recipient.as_ref().to_owned(),
+            subject: subject.to_owned(),
+            html_body: html_content.to_owned(),
+            text_body: text_content.to_owned(),
+        };
+
+        let builder = self
+            .http_client
+            .post(url)
+            .header(
+                "X-Postmark-Server-Token",
+                self.authorization_token.expose_secret(),
+            )
+            .json(&payload);
+
+        builder.send().await?;
+
+        Ok(())
+    }
+
+    pub fn new(
+        authorization_token: Secret<String>,
+        base_url: String,
+        sender: SubscriberEmail,
+    ) -> Self {
+        Self {
+            authorization_token,
+            http_client: Client::new(),
+            base_url,
+            sender,
+        }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::domain::SubscriberEmail;
+    use crate::email_client::EmailClient;
+    use fake::faker::internet::en::SafeEmail;
+    use fake::faker::lorem::en::{Paragraph, Sentence};
+    use fake::{Fake, Faker};
+    use secrecy::Secret;
+    use wiremock::matchers::any;
+    use wiremock::{Mock, MockServer, ResponseTemplate};
+
+    #[tokio::test]
+    async fn send_email_fires_a_request_to_base_url() {
+        // Arrange
+        let mock_server = MockServer::start().await;
+        let sender = SubscriberEmail::parse(SafeEmail().fake()).unwrap();
+        let email_client = EmailClient::new(Secret::new(Faker.fake()), mock_server.uri(), sender);
+
+        Mock::given(any())
+            .respond_with(ResponseTemplate::new(200))
+            .expect(1)
+            .mount(&mock_server)
+            .await;
+
+        let subscriber_email = SubscriberEmail::parse(SafeEmail().fake()).unwrap();
+        let subject = Sentence(1..2).fake::<String>();
+        let content = Paragraph(1..10).fake::<String>();
+
+        // Act
+        let _ = email_client
+            .send_email(subscriber_email, &subject, &content, &content)
+            .await;
+
+        // Assert
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
 use sqlx::PgPool;
 use std::net::TcpListener;
 use zero2prod::configuration::get_configuration;
+use zero2prod::email_client::EmailClient;
 use zero2prod::startup::run;
 use zero2prod::telemetry::{get_subscriber, init_subscriber};
 
@@ -12,9 +13,20 @@ async fn main() -> Result<(), std::io::Error> {
     // Panic if we cannot read configuration
     let configuration = get_configuration().expect("Failed to read configuration");
 
-    let pool = PgPool::connect_with(configuration.database.with_db())
+    let db_pool = PgPool::connect_with(configuration.database.with_db())
         .await
         .expect("Failed to connect to Postgres database");
+
+    let sender_email = configuration
+        .email_client
+        .sender()
+        .expect("Invalid sender email address.");
+
+    let email_client = EmailClient::new(
+        configuration.email_client.authorization_token,
+        configuration.email_client.base_url,
+        sender_email,
+    );
 
     let address = format!(
         "{}:{}",
@@ -23,7 +35,7 @@ async fn main() -> Result<(), std::io::Error> {
 
     let listener = TcpListener::bind(address)?;
 
-    let _ = run(listener, pool)?.await;
+    let _ = run(listener, db_pool, email_client)?.await;
 
     Ok(())
 }
